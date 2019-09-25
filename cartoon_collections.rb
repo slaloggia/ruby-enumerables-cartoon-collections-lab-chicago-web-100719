fe270158b71ca75df60a12dd0002afe0abf538df
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-25 20:28:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,4 +13,6 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  array.find{|obj| obj == "cheddar" || obj == "gouda" || obj == "camembert"}
 end
